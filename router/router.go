8e39e29a795990a37125d03bffe307d4ee1ea3f8
Prosecutor1x <krishnarjunnanda@gmail.com>
2023-07-10 12:36:07 +0530

    Changes

--- router/router.go
+++ router/router.go
@@ -29,8 +29,9 @@ func Router() *mux.Router {
 	// user routes
 	router.HandleFunc("/api/createUser", user_handler.CreateUserHandler).Methods("POST")
 	router.HandleFunc("/api/checkUser", user_handler.CheckUserExist).Methods("POST")
-	router.HandleFunc("/api/getUser/{id}", user_handler.GetUser).Methods("GET")
-	// router.Handle("/api/getUser/{id}", middleware.AuthMiddleware(http.HandlerFunc(user_handler.GetUser))).Methods("GET")
+	//router.HandleFunc("/api/getUser/{id}", user_handler.GetUser).Methods("GET")
+	router.Handle("/api/getUser/{id}", middleware.AuthMiddleware(http.HandlerFunc(user_handler.GetUser))).Methods("GET")
+	router.Handle("/api/updateUser/{id}", middleware.AuthMiddleware(http.HandlerFunc(user_handler.UpdateUser))).Methods("PUT")
 
 	// media routes
 	router.HandleFunc("/api/uploadMedia", media_handler.UploadMediaToS3).Methods("POST")
